398d4e51e8ab79a621f527132f14c4503939357b
Benjamin Garcia <zovbenja@hotmail.com>
2018-12-18 22:12:06 +0100

    componentes,dist,services elementos para crear web superheroes con reactjs aunque no se va a utilizar, se crea el desarrollo necesario para desplegar reactjs con proyecto de la forma indicada por msdn.

--- NodejsWebApp/serverexpress.js
+++ NodejsWebApp/serverexpress.js
@@ -1,5 +1,15 @@
-﻿var _express = require('express');
-var _colors = require('colors');
-const server = _express();
-server.get('/', function(req, res) { res.send('<h1>Fin del curso</h1>'); res.end(); })
-server.listen(3000, () => { console.log("estoy escuchando por el 3000 pare.".red);});
+﻿'use strict';
+var path = require('path');
+var express = require('express');
+
+var app = express();
+
+var staticPath = path.join(__dirname, '/');
+app.use(express.static(staticPath));
+
+// Allows you to set port in the project properties.
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), function () {
+    console.log('listening');
+});
